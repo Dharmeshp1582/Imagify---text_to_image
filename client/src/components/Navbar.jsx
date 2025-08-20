9fa609d8061198298ca00b6b4da1184ba6436b36
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-20 18:29:36 +0530

    login and signup add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const { user, setUser } = useContext(AppContext);
+  const { user, setShowLogin } = useContext(AppContext);
 
   const navigate = useNavigate();
 
@@ -43,8 +43,8 @@ const Navbar = () => {
               Pricing
             </p>
             <button
-              onClick={() => navigate("/login")}
-              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full"
+              onClick={() => setShowLogin(true)}
+              className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full cursor-pointer"
             >
               Login
             </button>
