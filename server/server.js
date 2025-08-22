abf65f4f1fa248be02f6ce87f15deb9f326a58d5
Dharmeshp1582 <alpeshpatelvirpur@gmail.com>
2025-08-22 09:31:39 +0530

    add vercel uri

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ['http://localhost:5173', 'https://imagify-text-to-image-mauve.vercel.app'],
+    credentials: true,
+  }
+));
 app.use(cookieParser());
 connectDb();
 
